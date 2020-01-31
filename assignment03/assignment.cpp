c1f9405d58c4f36370f190d18d8fa91dec9c0ca1
GeneralRR <robert.rudnev@rwth-aachen.de>
2020-01-31 21:51:11 +0100

    created folder structure

---
+++ assignment03/assignment.cpp
@@ -0,0 +1,20 @@
+/*
+ * Basics of Computer Graphics Exercise
+ */
+ 
+#include "assignment.h"
+using namespace std;
+
+void drawScene(int scene, float runTime) {
+  
+    drawCircle( glm::vec3(1.0f, 0.0f, 0.0f), glm::mat4() );
+}
+
+void initCustomResources()
+{
+}
+
+void deleteCustomResources()
+{
+}
+
